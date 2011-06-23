dc49906f9a940be1fdcd2e270e6db123e3340fdf
Shaun Ervine <shaun.ervine@gmail.com>
2011-06-23 10:08:40 +1000

    Created a basic iOS project that includes a target for unit tests

---
+++ JenkinsIOSExample/JenkinsIOSExampleTests/JenkinsIOSExampleTests.h
@@ -0,0 +1,17 @@
+//
+//  JenkinsIOSExampleTests.h
+//  JenkinsIOSExampleTests
+//
+//  Created by S on 23/06/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface JenkinsIOSExampleTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ JenkinsIOSExample/JenkinsIOSExampleTests/JenkinsIOSExampleTests.m
@@ -0,0 +1,33 @@
+//
+//  JenkinsIOSExampleTests.m
+//  JenkinsIOSExampleTests
+//
+//  Created by S on 23/06/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "JenkinsIOSExampleTests.h"
+
+
+@implementation JenkinsIOSExampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in JenkinsIOSExampleTests");
+}
+
+@end
